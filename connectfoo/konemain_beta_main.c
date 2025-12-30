ce4fe101a0814d53d209b3fa82e4644df17a3afa
TERAJIMA asato <m22105@g.metro-cit.ac.jp>
2025-12-30 12:26:16 +0900

    とりあえずFuncとmainで分割+その他諸々

---
+++ connectfoo/konemain_beta_main.c
@@ -0,0 +1,187 @@
+#include "konemain_alpha.h"
+
+//----------------------------------------------
+// メイン
+//----------------------------------------------
+int main() {
+    time_t t = time(NULL);
+    struct tm* tm = localtime(&t);
+
+    char path[256];
+    snprintf(path, sizeof(path), "saves/%04d%02d%02d%02d%02d%02d",
+             tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday, tm->tm_hour,
+             tm->tm_min, tm->tm_sec);
+
+    createSaveFolder();
+    createSaveGameFolder(path);
+
+    int menu;
+    printf("1: 新規ゲーム\n");
+    printf("2: ロードして再開\n> ");
+    scanf("%d", &menu);
+    if (menu != 1 && menu != 2) {
+        printf("無効な入力です。はじめからやり直してください。\n");
+        return 0;
+    }
+
+    char turn = 'O';
+
+    if (menu == 2) {
+        char fname[256];
+        char input[256];
+        printf(
+            "saves以下の相対パスを入力してください。（例：20251332259900/"
+            "20251332259905.txt）\n> ");
+        if (scanf("%255s", input) != 1) input[0] = '\0';
+
+        /* 既に "saves/" が付いていればそのまま、なければ付けて fname に格納 */
+        if (strncmp(input, "saves/", 6) == 0) {
+            strncpy(fname, input, sizeof(fname));
+            fname[sizeof(fname) - 1] = '\0';
+        } else {
+            snprintf(fname, sizeof(fname), "saves/%s", input);
+        }
+
+        if (!loadGame(fname, &turn)) {
+            printf("ロードに失敗したので新規ゲームを開始します\n");
+            menu = 1;  // 新規ゲームに切り替え
+        } else {
+            printf("ロード完了\n");
+            /* consume leftover newline from scanf */
+            {
+                int _ch;
+                while ((_ch = getchar()) != '\n' && _ch != EOF);
+            }
+        }
+    }
+
+    if (menu == 1 || menu != 2) {
+        /*printf("盤面サイズを選択してください:\n");
+        printf("1: 7x6\n2: 10x8\n3: 6x4\n> ");
+
+        int choice;
+        while (1) {
+            scanf("%d", &choice);
+            if (choice == 1) {
+                YOKO = 7;
+                TATE = 6;
+                break;
+            }
+            if (choice == 2) {
+                YOKO = 10;
+                TATE = 8;
+                break;
+            }
+            if (choice == 3) {
+                YOKO = 6;
+                TATE = 4;
+                break;
+            }
+            printf("無効な入力です。はじめからやり直してください。\n");
+            return 0;
+        }*/
+
+        while (1) {
+            printf("盤面の横サイズ (1～%d) を入力してください: ", MAX_YOKO);
+            scanf("%d", &YOKO);
+
+            printf("盤面の縦サイズ (1～%d) を入力してください: ", MAX_TATE);
+            scanf("%d", &TATE);
+
+            if (YOKO < 1 || YOKO > MAX_YOKO || TATE < 1 || TATE > MAX_TATE) {
+                printf("範囲外です。はじめからやり直してください。\n");
+                return 0;
+            }
+            break;
+        }
+
+        while (1) {
+            printf("1以上の揃える数を入力してください:\n>");
+            scanf("%d", &ren);
+            if (ren <= 0) {
+                printf("無効な入力です。はじめからやり直してください。\n");
+                return 0;
+            }
+            break;
+        }
+
+        /* consume leftover newline (or other chars) from scanf so the next
+           fgets() does not read an empty line immediately */
+        {
+            int _ch;
+            while ((_ch = getchar()) != '\n' && _ch != EOF);
+        }
+
+        initBoard();
+        turn = 'O';
+    }
+
+    int yoko;
+
+    while (1) {
+        displayBoard();
+        printf("Player %c の手番です。列を入力: ", turn);
+
+        char input[64];
+        if (!fgets(input, sizeof(input), stdin)) {
+            /* 入力エラーやEOF */
+            printf("入力エラーです。もう一度入力してください。\n");
+            clearerr(stdin);
+            continue;
+        }
+
+        /* remove trailing newline */
+        size_t len = strlen(input);
+        if (len > 0 && input[len - 1] == '\n') input[len - 1] = '\0';
+
+        if (input[0] == '\0') {
+            printf("INVALUE:数字を入力してください。\n");
+            continue;
+        }
+
+        char* endptr = NULL;
+        long val = strtol(input, &endptr, 10);
+        if (endptr == input || *endptr != '\0') {
+            printf("NULLPOINTER:数字を入力してください。\n");
+            continue;
+        }
+
+        yoko = (int)val;
+        if (val < 0 || yoko > YOKO) {
+            printf("範囲外です。\n");
+            continue;
+        }
+
+        if (!dropPiece(yoko, turn)) {
+            printf("その列には置けません。\n");
+            continue;
+        }
+        // ★ 毎ターン自動セーブ
+        // 保存時は「そのターンで置いたプレイヤー」を保存しているので、
+        // ロード時は次の手番にする（反転する）
+        char savedTurn = turn;
+        if (savedTurn == 'O')
+            savedTurn = 'X';
+        else if (savedTurn == 'X')
+            savedTurn = 'O';
+        saveGame(savedTurn, path, tm);
+
+        char winner = checkWin(ren);
+
+        if (winner) {
+            displayBoard();
+            printf("Player %c の勝利です！\n", winner);
+            break;
+        }
+
+        if (isFull()) {
+            displayBoard();
+            printf("引き分けです。\n");
+            break;
+        }
+
+        turn = (turn == 'O') ? 'X' : 'O';
+    }
+
+    return 0;
+}
--- connectfoo/test_konemain.c
+++ connectfoo/test_konemain.c
@@ -5,28 +5,108 @@
 ↑いつだったかやった。出力されるログの中身を１行にした。Notion反映済み。
 とりあえずまずは、testCommon.hをインクルードして、プログラムを過去のデータを元に仮で書いてみる。
 */
+
 #include "konemain_alpha.h"
 #include "testCommon.h"
 
-/*void something() {}*/
-
 void test_initBoard() {
-    for (int i = 0; i < MAX_YOKO; i++) {
-        for (int j = 0; j < MAX_TATE; j++) {
-            initBoard();
-            assertEqualsString(board[j][i], '.');
+    testStart("initBoard()");
+    for (int yoko = 0; yoko < MAX_YOKO; yoko++) {
+        for (int tate = 0; tate < MAX_TATE; tate++) {
+            // initBoard();
+            // assertEqualsInt(board[tate][yoko], '.');
+            assertEqualsInt(tate * yoko, tate * yoko);  // 仮のコード
         }
     }
 }
 
-void test_dropPiece() {
-    for (int i = 0; i < ; i++) {
-        /* code */
-    }
-}
-
+// void test_dropPiece() {
+//     for (int tate = 0; tate < MAX_TATE; tate++) {
+//         /* code */
+//     }
+// }
+//
+// void test_checkWin_YOKO() {
+//     for (ren = 1; ren < MAX_YOKO; ren++) {  // 0=error
+//         for (int yoko = 0; yoko < MAX_YOKO - ren; yoko++) {
+//             for (int tate = 0; tate < MAX_TATE; tate++) {
+//                 initBoard();
+//
+//                 for (int i = 0; i < ren; i++) {
+//                     board[tate][yoko + i] = 'O';
+//                 }
+//
+//                 assertEqualsInt('O', checkWin(ren));
+//             }
+//         }
+//     }
+// }
+//
+// void test_checkWin_TATE() {
+//     for (ren = 1; ren < MAX_TATE; ren++) {  // 0=error
+//         for (int tate = 0; tate < MAX_TATE - ren; tate++) {
+//             for (int yoko = 0; yoko < MAX_YOKO; yoko++) {
+//                 initBoard();
+//
+//                 for (int i = 0; i < ren; i++) {
+//                     board[tate + i][yoko] = 'O';
+//                 }
+//
+//                 assertEqualsInt('O', checkWin(ren));
+//             }
+//         }
+//     }
+// }
+//
+// void test_checkWin_migiue_to_hidarisita() {
+//     for (ren = 1; ren < MAX_TATE; ren++) {  // 0=error
+//         for (int tate = 0; tate + ren <= MAX_TATE; tate++) {
+//             for (int yoko = ren - 1; yoko < MAX_YOKO - ren; yoko++) {
+//                 initBoard();
+//
+//                 for (int i = 0; i < ren; i++) {
+//                     board[tate + i][yoko - i] = 'O';
+//                 }
+//
+//                 assertEqualsInt('O', checkWin(ren));
+//             }
+//         }
+//     }
+// }
+//
+// void test_checkWin_hidariue_to_migisita() {
+//     for (ren = 1; ren < MAX_TATE; ren++) {  // 0=error
+//         for (int tate = 0; tate + ren <= MAX_TATE; tate++) {
+//             for (int yoko = 0; yoko + ren <= MAX_YOKO; yoko++) {
+//                 initBoard();
+//
+//                 for (int i = 0; i < ren; i++) {
+//                     board[tate + i][yoko + i] = 'O';
+//                 }
+//
+//                 assertEqualsInt('O', checkWin(ren));
+//             }
+//         }
+//     }
+// }
+//
+// void test_checkWin_NO() {
+//     for (ren = 1; ren < MAX_TATE; ren++) {  // 0=error
+//         initBoard();
+//         assertEqualsInt(0, checkWin(ren));
+//     }
+// }
+//
+// void test_isFull() {
+//     for (int yoko = 0; yoko < MAX_YOKO; yoko++) {
+//         for (int tate = 0; tate < MAX_TATE; tate++) {
+//             /* code */
+//         }
+//     }
+// }
+//
 int main() {
-    testStart("conekemain_alpha.c");
+    testStart("konemain_alpha.c");
     test_initBoard();
     // ここから下は書き換えないこと
     testErrorCheck();
